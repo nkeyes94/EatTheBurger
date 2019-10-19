dc4a62ad182393790e8f44ea9b40e0e83e6c97dc
Nathan Keyes <n.keyes1994@gmail.com>
2019-10-19 15:47:30 -0400

    Pushing DB fixes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
+console.log("DB")
 
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 
@@ -13,5 +14,4 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
