de6cfa78afc15f08f4f9f21dae3acab9db6467be
Nathan Keyes <n.keyes1994@gmail.com>
2019-10-19 15:45:48 -0400

    Pushing server fixes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8000;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,8 +16,6 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller");
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
