c514a1509ab3032892b704a6911d546358b0664e
Nathan Keyes <n.keyes1994@gmail.com>
2019-10-19 15:28:57 -0400

    Changing DB var

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,9 @@
 var mysql = require("mysql");
 var connection;
 
-JAWSDB_URL = "mysql://rb7x7omwqrvxxjvj:g8gc7g19zssjjz11@vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ytrfbavxd69p14sp"
 
-
-if (JAWSDB_URL) {
-  connection = mysql.createConnection(JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else {
   connection = mysql.createConnection({
@@ -15,7 +13,6 @@ if (JAWSDB_URL) {
     user: "root",
     password: "coleakenta",
     database: "burgers_db"
-
   })
 }
 
